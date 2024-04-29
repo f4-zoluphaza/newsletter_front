07242cdb63dfac9d4c65905c2175e5fa099d4d15
sieun1002 <nata20034@naver.com>
2024-04-29 21:59:18 +0900

    [0429] sieun 로그인 페이지 완성

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,8 +5,23 @@ import {
   LogoImg,
   Form,
   Input,
+  LoginStateDiv,
+  CheckImg,
+  LoginStateP,
+  EyesImg,
+  Submit,
+  PWSearchSignUpDiv,
+  PWSearchSignUpP,
+  SocialLoginDiv,
+  SocialLoginImg,
+  Links
 } from "../styles/Login/LoginSignUp.styled";
+
 import Logo from "../images/Logo.svg";
+import Check from "../images/Login/Check.svg"
+import PWOpen from "../images/Login/PWOpen.svg"
+import KaTalk from "../images/Login/KaTalk.svg"
+import Naver from "../images/Login/Naver.svg"
 
 export default function Login() {
   return (
@@ -19,7 +34,32 @@ export default function Login() {
             id="password"
             placeholder="비밀번호 (숫자, 영문 8~12자리)"
           ></Input>
+
+          <EyesImg src={PWOpen}/>
+
+          <LoginStateDiv>
+            <CheckImg src={Check}/>
+            <LoginStateP>로그인 상태 유지</LoginStateP>
+          </LoginStateDiv>
+          <Submit value="로그인" type="submit"/>
         </Form>
+
+        <PWSearchSignUpDiv>
+          <Links to="/">
+            <PWSearchSignUpP>비밀번호 찾기</PWSearchSignUpP>
+          </Links>
+          
+          <PWSearchSignUpP>|</PWSearchSignUpP>
+          <Links to="SignUp">
+            <PWSearchSignUpP>회원가입</PWSearchSignUpP>
+          </Links>
+          
+        </PWSearchSignUpDiv>
+
+        <SocialLoginDiv>
+          <SocialLoginImg src={KaTalk}/>
+          <SocialLoginImg src={Naver}/>
+        </SocialLoginDiv>
       </WrapperDiv>
     </BodyDiv>
   );
--- src/styles/Login/LoginSignUp.styled.jsx
+++ src/styles/Login/LoginSignUp.styled.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const flexCenter = () => `
   display: flex;
@@ -14,7 +15,7 @@ export const BodyDiv = styled.div`
 export const WrapperDiv = styled.div`
   width: 510px;
   height: 100vh;
-  background-color: #fff8ec;
+  /* background-color: #fff8ec; */
   ${flexCenter()}
   flex-direction: column;
 `;
@@ -22,14 +23,19 @@ export const WrapperDiv = styled.div`
 export const LogoImg = styled.img`
   width: 256px;
   height: 200px;
+
+  margin-bottom: 35px;
 `;
 
 export const Form = styled.form`
   width: 510px;
   height: auto;
-  background-color: aqua;
+  /* background-color: aqua; */
   ${flexCenter()}
   flex-direction: column;
+  align-items: start;
+  position: relative; //eyesImg 때문에 설정
+
 `;
 
 export const Input = styled.input`
@@ -44,4 +50,94 @@ export const Input = styled.input`
 
   padding-left: 20px;
   box-sizing: border-box;
+`;
+
+export const EyesImg = styled.img`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  top: 85px;
+  right: 20px;
+
+  cursor: pointer;
+`
+
+export const LoginStateDiv = styled.div`
+  width: 200px;
+  height: 20px;
+  /* background-color: #b4ffb4; */
+  display: flex;
+  align-items: center;
+
+  margin-top: 10px;
+  margin-bottom: 30px;
+`
+
+export const CheckImg = styled.img`
+  width: 16px;
+  height: 16px;
+
+  cursor: pointer;
+`
+export const LoginStateP = styled.p`
+  font-size: 16px;
+  color: #767676;
+  margin: 0 0 0 5px;
+  padding: 0;
+`
+
+export const Submit = styled.input`
+  width: 508.48px;
+  height: 60px;
+  border-radius: 12px;
+  border: solid 0px;
+  background-color: #588539;
+  box-sizing: border-box;
+
+  ${flexCenter()}
+  color: #FFFFFF;
+  font-size: 21px;
+  font-weight: 600;
+
+  cursor: pointer;
+`
+
+export const PWSearchSignUpDiv = styled.div`
+  width: 250px;
+  height: 20px;
+  /* background-color: pink; */
+  ${flexCenter()}
+  justify-content: space-around;
+
+  margin-top: 30px;
+`
+
+export const PWSearchSignUpP = styled.p`
+  font-size: 16px;
+  color: #767676;
+
+  cursor: pointer;
+`
+
+export const SocialLoginDiv = styled.div`
+  width: 180px;
+  height: 90px;
+  /* background-color: bisque; */
+  ${flexCenter()}
+  justify-content: space-between;
+
+  margin-top: 50px;
+`
+
+export const SocialLoginImg = styled.img`
+  width: 80px;
+  height: 80px;
+
+  cursor: pointer;
+`
+
+export const Links = styled(Link)`
+  text-decoration: none;
+  color: #767676;
+  font-size: 16px;
 `;
