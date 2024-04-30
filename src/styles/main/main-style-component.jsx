269cb1d8fc935d0fc9c6fade6e3fc2991fdda626
jia0002 <2002jia@duksung.ac.kr>
2024-04-30 21:23:15 +0900

    [0430] jia 뉴스레터 신청 제작

--- src/styles/main/main-style-component.jsx
+++ src/styles/main/main-style-component.jsx
@@ -10,6 +10,7 @@ export const BodyDiv = styled.div`
   height: 100vh;
   /* background-color: beige; */
 `;
+
 export const NavDiv = styled.div`
   width: 1300px;
   height: 116px;
@@ -57,11 +58,87 @@ export const HeaderDiv = styled.div`
 `;
 
 export const ExplainDiv = styled.div`
-  width: 555px;
+  width: 460px;
   height: 107px;
   color: black;
-  margin-left: 190px;
-  margin-right: 190px;
-  margin-top: 20px;
-  background-color: blue;
+  margin-left: 90px;
+  margin-right: 50px;
+  margin-top: 60px;
+  /* background-color: blue; */
+  text-align: right;
+`;
+
+export const TitleSpan = styled.span`
+  display: block;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+export const TextSpan = styled.span`
+  display: block;
+  font-size: 20px;
+`;
+
+export const SubscribeDiv = styled.div`
+  width: 500px;
+  height: 210px;
+  padding-left: 3px;
+  /* background-color: lightblue; */
+`;
+
+export const Form = styled.form`
+  width: 500px;
+  height: 170px;
+  margin-bottom: 10px;
+  ${flexCenter()}
+  flex-direction: column;
+  align-items: start;
+  /* background-color: purple; */
+`;
+
+export const SmallTextSpan = styled.span`
+  display: block;
+  font-size: 15px;
+  font-weight: bolder;
+  display: inline;
+`;
+
+export const SubscribeInput = styled.input`
+  width: 430px;
+  height: 45px;
+  border-radius: 12px;
+  border: solid 2px #588539;
+  margin-bottom: 10px;
+  margin-left: 10px;
+
+  box-sizing: border-box;
+`;
+
+export const ButtonDiv = styled.div`
+  width: 500px;
+  height: 45px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  /* background-color: blue; */
+`;
+
+export const Button = styled.input`
+  width: 220px;
+  height: 45px;
+  border-radius: 12px;
+  border: solid 0px;
+  background-color: #588539;
+  box-sizing: border-box;
+
+  margin-left: 20px;
+  margin-right: 15px;
+
+  color: #000000;
+  font-size: 16px;
+  font-weight: 600;
+
+  cursor: pointer;
 `;
