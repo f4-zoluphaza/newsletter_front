cd6a0eb1eccdf8b3250113104c6f437c1b368620
jia0002 <2002jia@duksung.ac.kr>
2024-05-07 18:10:14 +0900

    [0507] jia 마이페이지 스크랩 완성

--- src/pages/myPage/Scrap.jsx
+++ src/pages/myPage/Scrap.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import { Div, BodyDiv } from '../../styles/Mypage_j/ScrapPage.styled.jsx';
+import {
+  Div,
+  BodyDiv,
+  TitleDiv,
+  Textspan,
+  PostAllDiv,
+  PostDiv,
+} from '../../styles/Mypage_j/ScrapPage.styled.jsx';
 
 import Header from '../../components/Header.jsx';
+import NewsletterPost from '../../components/NewsletterPost.jsx';
 
 export default function Scrap() {
   return (
@@ -9,6 +17,38 @@ export default function Scrap() {
       <BodyDiv>
         {/* 네비게이션바 */}
         <Header></Header>
+
+        <TitleDiv>
+          <Textspan>내 스크랩 목록</Textspan>
+        </TitleDiv>
+
+        {/* 스크랩 게시물 */}
+        <PostAllDiv>
+          <PostDiv>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+          </PostDiv>
+        </PostAllDiv>
+
+        <PostAllDiv>
+          <PostDiv>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+          </PostDiv>
+        </PostAllDiv>
+
+        {/* <PostAllDiv>
+          <PostDiv>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+          </PostDiv>
+        </PostAllDiv> */}
       </BodyDiv>
     </Div>
   );
--- src/styles/Mypage_j/ScrapPage.styled.jsx
+++ src/styles/Mypage_j/ScrapPage.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Div = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: auto;
 
   display: flex;
   justify-content: center;
@@ -10,6 +10,44 @@ export const Div = styled.div`
 
 export const BodyDiv = styled.div`
   width: 90vw;
-  height: 100vh;
+  height: auto;
   /* background-color: beige; */
+`;
+
+export const TitleDiv = styled.div`
+  width: 100%;
+  height: 80px;
+  margin-bottom: 15px;
+
+  display: flex;
+  justify-content: center;
+
+  /* background-color: lightpink; */
+`;
+
+export const Textspan = styled.div`
+  font-size: 48px;
+  font-weight: 700;
+`;
+
+export const PostAllDiv = styled.div`
+  width: 100%;
+  height: auto;
+  margin-bottom: 50px;
+
+  display: flex;
+  justify-content: center;
+
+  /* background-color: lightslategrey; */
+`;
+
+export const PostDiv = styled.div`
+  /* width: 85%; //컴퓨터 */
+  width: 97%;
+  height: 353px;
+
+  display: flex;
+  justify-content: space-evenly;
+
+  /* background-color: lightyellow; */
 `;
