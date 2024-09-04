341174f3d31a1626797175e81247cf73efd00390
sieun1002 <nata20034@naver.com>
2024-09-04 11:55:12 +0900

    [0904] sieun 관리자 페이지 (메인) 틀 작업 (12:00 pm 시은 노트북 작업)

--- src/styles/adminPage/Admin.styled.jsx
+++ src/styles/adminPage/Admin.styled.jsx
@@ -10,6 +10,69 @@ const flexCenter = () => `
 export const WrapperDiv = styled.div`
   width: 1220px;
   height: auto;
-
   background-color: beige;
 `;
+
+export const WrapperBox = styled.div`
+  /* background-color: #96cdfc; */
+  width: 80%;
+  height: 60vh;
+  border-top: 2px solid #588539;
+
+  margin-bottom: 40px;
+`;
+
+export const EachBox = styled.div`
+  /* background-color: darkgoldenrod; */
+  width: 100%;
+  height: 9.5%;
+  border-bottom: 2px solid #588539;
+  display: flex;
+`;
+
+export const CheckBox = styled.div`
+  width: 4%;
+  height: 100%;
+  border-right: 2px solid #588539;
+  border-left: 2px solid #588539;
+  /* background-color: azure; */
+  ${flexCenter()}
+`;
+
+export const NumBox = styled.div`
+  width: 4%;
+  height: 100%;
+  border-right: 2px solid #588539;
+  /* background-color: aquamarine; */
+  color: #588539;
+  font-size: 18px;
+  ${flexCenter()};
+`;
+
+export const TitleBox = styled.div`
+  width: 80%;
+  height: 100%;
+  border-right: 2px solid #588539;
+  font-size: 20px;
+  ${flexCenter()};
+  justify-content: start;
+
+  padding-left: 20px;
+`;
+
+export const DateBox = styled.div`
+  width: 12%;
+  height: 100%;
+  border-right: 2px solid #588539;
+  /* background-color: bisque; */
+  color: #588539;
+  font-size: 18px;
+  ${flexCenter()};
+`;
+
+export const CheckImg = styled.img`
+  width: 25px;
+  height: 25px;
+`;
+
+// export const DeleteBt = styled.button;
