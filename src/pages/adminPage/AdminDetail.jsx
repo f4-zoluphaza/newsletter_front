234bfd46c5d9e1f45980cf82b75a68ffe392defb
sieun1002 <nata20034@naver.com>
2024-09-06 14:38:38 +0900

    [0906] sieun AdminDetail css 비율 (노트북)

--- src/pages/adminPage/AdminDetail.jsx
+++ src/pages/adminPage/AdminDetail.jsx
@@ -1,57 +1,64 @@
 import React, { useState } from "react";
 import Header from "../../components/Header";
 // import { BodyDiv } from "../../styles/Mypage_s/Attendance.styled";
+import { Div, BodyDiv } from "../../styles/main/main-style-component.jsx";
 import {
-  Div,
-  BodyDiv
-} from "../../styles/main/main-style-component.jsx";
-import {Form, Label, Input, Textarea, TextareaWrapper,TextareaDiv, FixTextarea, Button, ButtonDiv, Links} from "../../styles/adminPage/AdminDetail.styled"
-
-
+  Form,
+  Label,
+  Input,
+  Textarea,
+  TextareaWrapper,
+  TextareaDiv,
+  Button,
+  ButtonDiv,
+  Links,
+} from "../../styles/adminPage/AdminDetail.styled";
 
 export default function Admin() {
   const [validCheck, setValidCheck] = useState(false);
   return (
     <Div>
       <BodyDiv>
-          <Header />
+        <Header />
 
-      <Form>
-        <Label for="title">제목</Label>
-        <Input type="text" id="title" name="title" required></Input>
+        <Form>
+          <Label for="title">제목</Label>
+          <Input type="text" id="title" name="title" required></Input>
 
-        <Label for="title">링크</Label>
-        <Input type="text" id="title" name="title" value=""></Input>
+          <Label for="title">링크</Label>
+          <Input type="text" id="title" name="title" value=""></Input>
 
-        <TextareaWrapper>
-          <TextareaDiv>
+          <TextareaWrapper>
+            <TextareaDiv>
               <Label for="title">원본</Label>
-              <Textarea value=""></Textarea> 
-          </TextareaDiv>
+              <Textarea value=""></Textarea>
+            </TextareaDiv>
 
-          <TextareaDiv>
+            <TextareaDiv>
               <Label for="title">육하원칙</Label>
-              <Textarea value=""></Textarea> 
-          </TextareaDiv>
+              <Textarea value=""></Textarea>
+            </TextareaDiv>
 
-          <TextareaDiv>
+            <TextareaDiv>
               <Label for="title">내용</Label>
-              <Textarea></Textarea> 
-          </TextareaDiv>
+              <Textarea></Textarea>
+            </TextareaDiv>
+          </TextareaWrapper>
 
-        </TextareaWrapper>
-        
-        <ButtonDiv>
-          <Links to="/Admin">
-            <Button type="button">취소</Button>
-          </Links>
+          <ButtonDiv>
+            <Links to="/Admin">
+              <Button type="button">취소</Button>
+            </Links>
 
-          <Button type="submit" backgroundColor="#588539">발행</Button>
-          
-          <Button type="submit" backgroundColor="#588539">저장</Button>
-        </ButtonDiv>
-        
-      </Form>
+            <Button type="submit" backgroundColor="#588539">
+              발행
+            </Button>
+
+            <Button type="submit" backgroundColor="#588539">
+              저장
+            </Button>
+          </ButtonDiv>
+        </Form>
       </BodyDiv>
     </Div>
   );
--- src/styles/adminPage/AdminDetail.styled.jsx
+++ src/styles/adminPage/AdminDetail.styled.jsx
@@ -9,67 +9,63 @@ const flexCenter = () => `
 
 export const Form = styled.form`
   width: 80%;
-  height: 70vh;
-  margin: 50px auto;
+  height: 800px;
+  margin: 20px auto;
 
   /* background-color: beige; */
-`
+`;
 
 export const Label = styled.label`
   display: block;
   margin-bottom: 5px;
   font-weight: bold;
-`
+`;
 
 export const Input = styled.input`
-    width: 100%;
-    padding: 10px;
-    margin-bottom: 20px;
-    border: 1px solid black;
-    box-sizing: border-box;
-`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 20px;
+  border: 1px solid black;
+  box-sizing: border-box;
+`;
 
 export const TextareaWrapper = styled.div`
   width: 100%;
-  height: 80%;
+  height: 70%;
   ${flexCenter()}
   justify-content: space-between;
-  margin-bottom: 10px;
-  
-  /* background-color: beige; */
-`
-export const TextareaDiv = styled.div`
-    width: 30%;
-    height: 95%;
+  /* margin-bottom: 10px; */
 
-    /* border: 1px solid black;
+  /* background-color: pink; */
+`;
+export const TextareaDiv = styled.div`
+  width: 30%;
+  height: 95%;
+
+  /* border: 1px solid black;
     box-sizing: border-box;
     resize: none; */
 
-    /* background-color: azure; */
-`
+  /* background-color: azure; */
+`;
 export const Textarea = styled.textarea`
-    width: 100%;
-    height: 95%;
+  width: 100%;
+  height: 95%;
 
-    box-sizing: border-box;
-    resize: none;
-`
-
-export const FixTextarea = styled.div`
-    width: 100%;
-    height: 95%;
-
-    border: 1px solid black;
-    box-sizing: border-box;
-`
+  box-sizing: border-box;
+  resize: none;
+`;
 
 export const ButtonDiv = styled.div`
   width: 100%;
-  height: 30px;
+  height: 40px;
   ${flexCenter()}
   justify-content: end;
-`
+
+  margin: 10px 0;
+
+  /* background-color: aquamarine; */
+`;
 
 export const Button = styled.button`
   width: 100px;
@@ -80,10 +76,10 @@ export const Button = styled.button`
   margin-left: 15px;
 
   background-color: ${(props) => props.backgroundColor || "#C0C0C0"};
-  color: #FFFFFF;
+  color: #ffffff;
 
   cursor: pointer;
-`
+`;
 
 export const Links = styled(Link)`
   text-decoration: none;
