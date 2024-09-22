704a8d312cf2909d9834b197a3265f4553bdaaaf
jia0002 <2002jia@duksung.ac.kr>
2024-09-22 21:36:13 +0900

    [0922] jia 스크랩한 뉴스 목록 불러오기 api 연결 중

--- src/pages/myPage/Scrap.jsx
+++ src/pages/myPage/Scrap.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../api/api.js';
+
+import { useParams } from 'react-router-dom';
+
 import {
   Div,
   BodyDiv,
@@ -7,11 +11,64 @@ import {
   PostAllDiv,
   PostDiv,
 } from '../../styles/Mypage_j/ScrapPage.styled.jsx';
+import { Links } from '../../styles/main/main-style-component.jsx';
 
 import Header from '../../components/Header.jsx';
 import NewsletterPost from '../../components/NewsletterPost.jsx';
 
 export default function Scrap() {
+  const [paginationNum, setPaginationNum] = useState(1);
+  const [totalPages, setTotalPages] = useState();
+  const [data, setData] = useState({});
+
+  const { id } = useParams();
+
+  // 쿠키 값 읽는 함수
+  function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+    return null;
+  }
+
+  // 스크랩한 뉴스 목록 불러오기 api 함수
+  const scrappedNewsAPI = async () => {
+    try {
+      const url = `api/v1/news/scrapped`;
+
+      // 쿠키에서 'jwtToken' 값을 가져옴
+      const token = getCookie('jwtToken');
+
+      const response = await api.get(url, {
+        headers: {
+          'Content-Type': 'application/json',
+          'ngrok-skip-browser-warning': '69420',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log(response.data.items);
+
+      const result = response.data.items;
+      setData(result);
+      // const totalPages = response.data.totalPages;
+      // setTotalPages(totalPages);
+    } catch (error) {
+      console.error(
+        '스크랩한 뉴스 목록 불러오기 에러',
+        error.response ? error.response.data : error
+      );
+    }
+  };
+
+  // useEffect(() => {
+  //   scrappedNewsAPI();
+  // }, [paginationNum]);
+
+  useEffect(() => {
+    scrappedNewsAPI();
+  });
+
   return (
     <Div>
       <BodyDiv>
@@ -22,26 +79,46 @@ export default function Scrap() {
           <Textspan>내 스크랩 목록</Textspan>
         </TitleDiv>
 
-        {/* 스크랩 게시물 */}
+        {/* 뉴스레터 게시물 */}
         <PostAllDiv>
-          <PostDiv>
-            <NewsletterPost></NewsletterPost>
-            <NewsletterPost></NewsletterPost>
-            <NewsletterPost></NewsletterPost>
-            <NewsletterPost></NewsletterPost>
-          </PostDiv>
+          {data.length > 0 &&
+            data.map((item, index) => (
+              <Links
+                to={{
+                  pathname: `/detailPage/${item.id}`,
+                }}
+              >
+                <NewsletterPost
+                  key={item.id}
+                  title={item.title}
+                  content={item.content}
+                  publishDate={item.publishDate}
+                  thumbnail={item.thumbnail}
+                ></NewsletterPost>
+              </Links>
+            ))}
         </PostAllDiv>
 
-        <PostAllDiv>
-          <PostDiv>
-            <NewsletterPost></NewsletterPost>
-            <NewsletterPost></NewsletterPost>
-            <NewsletterPost></NewsletterPost>
-            <NewsletterPost></NewsletterPost>
-          </PostDiv>
-        </PostAllDiv>
-
+        {/* 스크랩 게시물 예시*/}
         {/* <PostAllDiv>
+          <PostDiv>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+          </PostDiv>
+        </PostAllDiv>
+
+        <PostAllDiv>
+          <PostDiv>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+            <NewsletterPost></NewsletterPost>
+          </PostDiv>
+        </PostAllDiv>
+
+        <PostAllDiv>
           <PostDiv>
             <NewsletterPost></NewsletterPost>
             <NewsletterPost></NewsletterPost>
