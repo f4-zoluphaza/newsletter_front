6b6a9877d40ddf5084e13c0f0ae9751c2c8bdbd6
sieun1002 <nata20034@naver.com>
2024-09-22 21:16:55 +0900

    [0922] sieun baseURL 변경 (imagBaseURL 포함)

--- src/api/api.js
+++ src/api/api.js
@@ -11,11 +11,12 @@ const api = axios.create({
 
   // 임시 url 2
   // baseURL: 'https://humble-commonly-goshawk.ngrok-free.app/',
+  // imgBaseURL: "https://humble-commonly-goshawk.ngrok-free.app",
 
   // 임시 url 3
   baseURL: "https://catfish-solid-specially.ngrok-free.app/",
 
-  imgBaseURL: "https://humble-commonly-goshawk.ngrok-free.app",
+  imgBaseURL: "https://catfish-solid-specially.ngrok-free.app",
 });
 
 export default api;
