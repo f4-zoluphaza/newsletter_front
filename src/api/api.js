fb012c7aba335bfa6046b4ea1ddecaa62827d0d5
sieun1002 <nata20034@naver.com>
2024-09-25 11:50:29 +0900

    [0925] sieun 구독해지 완료

--- src/api/api.js
+++ src/api/api.js
@@ -1,16 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
   //원래 url
   // baseURL: 'https://humble-commonly-goshawk.ngrok-free.app/',
 
   // 임시 url 다운
-  // baseURL: 'https://catfish-solid-specially.ngrok-free.app/',
-  // imgBaseURL: 'https://catfish-solid-specially.ngrok-free.app',
+  // baseURL: "https://catfish-solid-specially.ngrok-free.app/",
+  // imgBaseURL: "https://catfish-solid-specially.ngrok-free.app",
 
   // 임시 url 2 나영
-  baseURL: 'https://humble-commonly-goshawk.ngrok-free.app/',
-  imgBaseURL: 'https://humble-commonly-goshawk.ngrok-free.app',
+  baseURL: "https://humble-commonly-goshawk.ngrok-free.app/",
+  imgBaseURL: "https://humble-commonly-goshawk.ngrok-free.app",
 });
 
 export default api;
--- src/pages/myPage/Unsubscribe.jsx
+++ src/pages/myPage/Unsubscribe.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import api from '../../api/api.js'
+import React, { useState } from "react";
+import api from "../../api/api.js";
 import { useNavigate } from "react-router-dom";
 
 import {
   BodyDiv,
   WrapperDiv,
   LogoImg,
+  Input,
 } from "../../styles/Login/LoginSignUp.styled";
 
 import {
@@ -21,31 +22,37 @@ import Logo from "../../images/Logo.svg";
 
 export default function Unsubscribe() {
   const navigate = useNavigate();
+  const [vaildEmail, setVaildEmail] = useState(null);
 
-  // 쿠키 값 읽는 함수
-  function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(";").shift();
-    return null;
-  }
+  const [form, setForm] = useState({
+    email: "",
+  });
+
+  const handleEmailChange = (e) => {
+    const email = e.target.value;
+    setForm({ ...form, email });
+
+    if (email.trim() === "") {
+      setVaildEmail(null);
+    }
+  };
 
   const handleUnsubscribeAPI = async () => {
     try {
-      const url =
-        "api/v1/mypage/unsubscribe";
-      
-        // 쿠키에서 'jwtToken' 값을 가져옴
-      const token = getCookie("jwtToken");
-      const response = await api.delete(url, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const url = `api/v1/mypage/unsubscribe?email=${form.email}`;
+
+      const response = await api.post(
+        url,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       navigate("/Mypage");
-      // console.log(response.data);
+      console.log(response.data);
     } catch (error) {
       console.error(
         "구독해지 error",
@@ -56,7 +63,7 @@ export default function Unsubscribe() {
   return (
     <BodyDiv>
       <WrapperDiv height="67vh" justifyContent="start">
-        <LogoImg src={Logo} marginBottom="20px" />
+        <LogoImg src={Logo} marginBottom="0px" />
         <MainP color="#588539" fontWeight="600" fontSize="33px" margin="0">
           뉴스레터 구독해지
         </MainP>
@@ -66,13 +73,40 @@ export default function Unsubscribe() {
             color="#E74F4F"
             fontWeight="600"
             fontSize="17px"
-            margin="0 0 20px 10px"
+            margin="10px 0 -2px 10px"
           >
-            구독 해지하시겠어요?
+            구독 해지
+            <span style={{ color: "black", fontWeight: "600" }}>
+              하시겠다면
+            </span>
           </MainP>
+          <MainP fontWeight="600" fontSize="17px" margin="0 0 10px 10px">
+            아래에 구독 시 신청한 이메일을 작성해주세요.
+          </MainP>
+          <Input
+            width="400px"
+            height="60px"
+            borderRadius="10px"
+            style={{
+              marginLeft: "10px",
+              marginBottom: "20px",
+              borderColor: "#588539",
+            }}
+            placeholder="ex) example@naver.com"
+            id="email"
+            type="email"
+            value={form.email}
+            onChange={handleEmailChange}
+          />
           <ButtonDiv>
             <Links to="/Mypage">
-              <Button backgroudColor="#588539" color="#FFFFFF" textDecoration="underline">구독 유지하기</Button>
+              <Button
+                backgroudColor="#588539"
+                color="#FFFFFF"
+                textDecoration="underline"
+              >
+                구독 유지하기
+              </Button>
             </Links>
             <Button
               onClick={() => {
