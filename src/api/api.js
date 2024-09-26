1ad888403f7de033e399bc60300e865e8621079a
jia0002 <2002jia@duksung.ac.kr>
2024-09-26 22:47:54 +0900

    [0926] sieun baseURL 변경

--- src/api/api.js
+++ src/api/api.js
@@ -5,12 +5,12 @@ const api = axios.create({
   // baseURL: 'https://humble-commonly-goshawk.ngrok-free.app/',
 
   // 임시 url 다운
-  // baseURL: 'https://catfish-solid-specially.ngrok-free.app/',
-  // imgBaseURL: 'https://catfish-solid-specially.ngrok-free.app',
+  baseURL: 'https://catfish-solid-specially.ngrok-free.app/',
+  imgBaseURL: 'https://catfish-solid-specially.ngrok-free.app',
 
   // 임시 url 2 나영
-  baseURL: 'https://humble-commonly-goshawk.ngrok-free.app/',
-  imgBaseURL: 'https://humble-commonly-goshawk.ngrok-free.app',
+  // baseURL: 'https://humble-commonly-goshawk.ngrok-free.app/',
+  // imgBaseURL: 'https://humble-commonly-goshawk.ngrok-free.app',
 });
 
 export default api;
